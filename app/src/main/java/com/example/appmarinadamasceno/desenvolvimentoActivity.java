704b4a280de9433adb3719231444f98c91e6be21
MarinaBohlant <marinabohlant@etec.sp.gov.br>
2023-09-14 20:27:08 -0300

    primeiro commit

---
+++ app/src/main/java/com/example/appmarinadamasceno/desenvolvimentoActivity.java
@@ -0,0 +1,14 @@
+package com.example.appmarinadamasceno;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class desenvolvimentoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_desenvolvimento);
+    }
+}
