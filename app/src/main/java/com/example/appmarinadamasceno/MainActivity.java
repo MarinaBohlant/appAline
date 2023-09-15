6d0be75ea78af1c6aafd05a4480b7e537923d8e4
guiguiba0202 <guilhermeevangelistadasilva6@gmail.com>
2023-09-15 08:39:20 -0300

    Commit 2

--- app/src/main/java/com/example/appmarinadamasceno/MainActivity.java
+++ app/src/main/java/com/example/appmarinadamasceno/MainActivity.java
@@ -2,14 +2,22 @@ package com.example.appmarinadamasceno;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        findViewById(R.id.btnentrar);
+    }
+    public void Login(View v){
+        Intent intentMain = new Intent(MainActivity.this, loginActivity.class);
+        startActivity(intentMain);
     }
 }
--- app/src/main/java/com/example/appmarinadamasceno/loginActivity.java
+++ app/src/main/java/com/example/appmarinadamasceno/loginActivity.java
@@ -2,7 +2,9 @@ package com.example.appmarinadamasceno;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class loginActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class loginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        findViewById(R.id.btnlogin);
+    }
+    public void Logado(View v) {
+        Intent intentLogin = new Intent(loginActivity.this, LogadoActivity.class);
+        startActivity(intentLogin);
     }
 }
