98586bc717b9ceaa2eed47f4cbd38fd54ea9ee3e
MathDamas <matheus.santos1608@etec.sp.gov.br>
2023-09-21 21:07:54 -0300

    Atualizações do aplicativo novamente N°3 provavelmente

--- app/src/main/java/com/example/appmarinadamasceno/FimActivity.java
+++ app/src/main/java/com/example/appmarinadamasceno/FimActivity.java
@@ -3,6 +3,7 @@ package com.example.appmarinadamasceno;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -20,7 +21,8 @@ public class FimActivity extends AppCompatActivity {
         startActivity(intentFim);
     }
     public void Site(View v) {
-        Intent intentSite = new Intent(FimActivity.this, siteActivity.class);
-        startActivity(intentSite);
+        Uri web = Uri.parse("https://www.portaldomeioambiente.com.br");
+        Intent siteIntent = new Intent(Intent.ACTION_VIEW, web);
+        startActivity(siteIntent);
     }
 }
--- app/src/main/java/com/example/appmarinadamasceno/mapaActivity.java
+++ app/src/main/java/com/example/appmarinadamasceno/mapaActivity.java
@@ -2,7 +2,10 @@ package com.example.appmarinadamasceno;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class mapaActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class mapaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapa);
+
+        findViewById(R.id.mapView);
+    }
+    public void Mapa(View v) {
+        Uri local = Uri.parse("geo:-15.5683531,-56.0722212?z=17");
+        Intent mapaIntent = new Intent(Intent.ACTION_VIEW, local);
+        startActivity(mapaIntent);
     }
 }
